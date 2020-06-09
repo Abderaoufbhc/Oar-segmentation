35c5e9e4adb69ba3db3961e437ece0fd465bd09e
Abderaoufbhc <33267364+Abderaoufbhc@users.noreply.github.com>
2020-06-09 20:33:57 +0200

    Create U-net.py

---
+++ U-net.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import backend as K
+from keras.layers import Input, Activation, Conv2D, Flatten, Dense, MaxPooling2D, Dropout, Add, LeakyReLU, UpSampling2D
+from keras.models import Model, load_model
+import tensorflow as tf
+from keras.callbacks import ReduceLROnPlateau
+
+x_train = np.load('drive/My Drive/dataset_Heart/x_train.npy')
+y_train = np.load('drive/My Drive/dataset_Heart/y_train.npy')
+x_val = np.load('drive/My Drive/dataset_Heart/x_val.npy')
+y_val = np.load('drive/My Drive/dataset_Heart/y_val.npy')
+
+print(x_train.shape, y_train.shape)
+print(x_val.shape, y_val.shape)
+
+
+
+inputs = tf.keras.layers.Input((256, 256, 1))
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+c1 = tf.keras.layers.Dropout(0.1)(c1)
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+c2 = tf.keras.layers.Dropout(0.1)(c2)
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+ 
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+c3 = tf.keras.layers.Dropout(0.2)(c3)
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+ 
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+c4 = tf.keras.layers.Dropout(0.2)(c4)
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+ 
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+c5 = tf.keras.layers.Dropout(0.3)(c5)
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+#decoder path 
+u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+u6 = tf.keras.layers.concatenate([u6, c4])
+c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+c6 = tf.keras.layers.Dropout(0.2)(c6)
+c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+ 
+u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+u7 = tf.keras.layers.concatenate([u7, c3])
+c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+c7 = tf.keras.layers.Dropout(0.2)(c7)
+c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+ 
+u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+u8 = tf.keras.layers.concatenate([u8, c2])
+c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+c8 = tf.keras.layers.Dropout(0.1)(c8)
+c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+ 
+u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+c9 = tf.keras.layers.Dropout(0.1)(c9)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+ 
+outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc', 'mse'])
+
+checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_OAR.h5', verbose=1, save_best_only=True)
+
+callbacks = [
+        tf.keras.callbacks.TensorBoard(log_dir='logs')]
+
+model.summary()
+history = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=250, batch_size=16, callbacks=callbacks)
+model.save('heart_segmentation.model')
+
+
+
+fig, ax = plt.subplots(2, 2, figsize=(10, 7))
+
+ax[0, 0].set_title('loss')
+ax[0, 0].plot(history.history['loss'], 'r')
+ax[0, 1].set_title('acc')
+ax[0, 1].plot(history.history['acc'], 'b')
+
+ax[1, 0].set_title('val_loss')
+ax[1, 0].plot(history.history['val_loss'], 'r--')
+ax[1, 1].set_title('val_acc')
+ax[1, 1].plot(history.history['val_acc'], 'b--')
+
+
+
+preds = model.predict(x_val)
+
+
+for i, pred in enumerate(preds):
+  fig, ax = plt.subplots(1, 3)
+  ax[0].imshow(x_val[i].squeeze(), cmap='gray')
+  ax[1].imshow(y_val[i].squeeze(), cmap='gray')
+  ax[2].imshow(pred.squeeze(), cmap='gray')
